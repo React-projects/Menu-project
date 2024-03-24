f057db907cf62eb1a8fc6bcaafc419b70caa907c
BAssal <baslm8496@gmail.com>
2024-03-25 01:22:45 +0200

    adding images and editing api data

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import Header from "./Components/Header";
 import MainTitle from "./Components/MainTitle";
 import Categories from "./Components/Categories";
 import CardItem from "./Components/CardItem";
+import { useState } from "react";
+import { items } from "./api/data";
 
 function App() {
+  const [itemData, setItemData] = useState(items);
+  // ! Filter by category
+
+  const filterByCategory = (cat) => {
+
+    if(cat ==='الكل'){
+      setItemData(items)
+    }else{
+      const newArray = items.filter((item) => item.category === cat);
+      setItemData(newArray);
+
+    }
+  };
   return (
     <div className="font color-body">
-        <Header/>
+      <Header />
       <Container>
-        <MainTitle/>
-        <Categories/>
-        <CardItem/>
-        <CardItem/>
-        <CardItem/>
-        <CardItem/>
-        <CardItem/>
-        
+        <MainTitle />
+        <Categories filterByCategory={filterByCategory} />
+        <CardItem itemData={itemData} />
       </Container>
-      
     </div>
   );
 }
---
+++ src/api/data.js
@@ -0,0 +1,8 @@
+export const items = [
+    { id: Math.random(), title: 'وجبه فطار', description: 'وصف الفطار', price: '100 جنيه', category: 'فطار', imgUrl: "/images/1.png" },
+    { id: Math.random(), title: 'وجبه غدا', description: 'وصف الفطار', price: '80 جنيه', category: 'غدا', imgUrl: "/images/2.png" },
+    { id: Math.random(), title: 'وجبه عشاء', description: 'وصف الفطار', price: '40 جنيه', category: 'عشاء', imgUrl: "/images/3.png" },
+    { id: Math.random(), title: 'وجبه غدا ممتاز', description: 'وصف الفطار', price: '30 جنيه', category: 'غدا', imgUrl: "/images/4.jpg" },
+    { id: Math.random(), title: 'وجبه عشاء درجه اولى', description: 'وصف الفطار', price: '50 جنيه', category: 'عشاء', imgUrl: "/images/1.png" },
+    { id: Math.random(), title: 'وجبه فطار', description: 'وصف الفطار', price: '50 جنيه', category: 'فطار', imgUrl: "/images/5.jpg" },
+    { id: Math.random(), title: 'وجبه سناك', description: 'وصف الفطار', price: '50 جنيه', category: 'سناك', imgUrl: "/images/6.jpg" }]
