0721e8f5efa1df167473848ecda8a0ecdf4c5a67
BAssal <baslm8496@gmail.com>
2024-03-25 01:23:27 +0200

    funcatllity category filter

--- src/Components/CardItem.jsx
+++ src/Components/CardItem.jsx
@@ -1,26 +1,36 @@
 import { Col, Row } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 
-function CardItem() {
+function CardItem({ itemData }) {
   return (
     <div>
       <Row>
-        <Col sm="12" className="mb-3">
-          <Card className="d-flex flex-row" style={{background:' #F8F8F8'}}>
-            <Card.Img className="img-item" src="/images/1.png" />
-            <Card.Body>
-              <Card.Title className="d-flex justify-content-between">
-                <div className="item-title"> Card Title</div>
-                <div className="item-price"> 50 polpol</div>
-              </Card.Title>
-              <Card.Text className="py-2">
-                <div className="item-description"></div>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
+        {itemData.length > 1 ? (
+          itemData.map((item) => {
+            return (
+              <Col sm="12" className="mb-3"    key={item.id}>
+                <Card       
+                  className="d-flex flex-row"
+                  style={{ background: " #F8F8F8" }}
+         
+                >
+                  <Card.Img className="img-item" src={item.imgUrl} />
+                  <Card.Body>
+                    <Card.Title className="d-flex justify-content-between">
+                      <div className="item-title"> {item.title}</div>
+                      <div className="item-price">{item.price}</div>
+                    </Card.Title>
+                    <Card.Text className="py-2">
+                      <div className="item-description">{item.description}</div>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })
+        ) : (
+          <h3> لا يوجد اصناف</h3>
+        )}
       </Row>
     </div>
   );
--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,6 +1,9 @@
 import { Col, Row } from "react-bootstrap";
 
-function Categories() {
+function Categories({ filterByCategory }) {
+  const onFilter = (cat) => {
+    filterByCategory(cat);
+  };
   return (
     <div>
       <Row className="my-2 mb-5">
@@ -8,24 +11,28 @@ function Categories() {
           <div className=" title  d-flex justify-content-center text-center">
             <div>
               <button
+                onClick={() => onFilter("الكل")}
                 style={{ border: "1px solid #b45b35" }}
                 className="btn mx-2"
               >
                 الكل
               </button>
               <button
+                onClick={() => onFilter("فطار")}
                 style={{ border: "1px solid #b45b35" }}
                 className="btn mx-2"
               >
                 فطار
               </button>
               <button
+                onClick={() => onFilter("غدا")}
                 style={{ border: "1px solid #b45b35" }}
                 className="btn mx-2"
               >
                 غداء{" "}
               </button>
               <button
+                onClick={() => onFilter("عشاء")}
                 style={{ border: "1px solid #b45b35" }}
                 className="btn mx-2"
               >
